4cdf556f9ec28f278e1ba07899f8b08c76e82907
hcoutant <coutanth@gmail.com>
2018-03-18 01:00:34 +0100

    Ajout joueur.h

---
+++ src/joueur.h
@@ -0,0 +1,25 @@
+#ifndef __JOUEUR_H__
+#define __JOUEUR_H__
+
+#include "jeu.h"
+#include "Cartes/carte.h"
+
+class Jeu; //Eviter les inclusions cyclique 
+class Carte;
+
+class Joueur {
+	private:
+		Jeu* j;
+		bool en_vie;
+		bool est_protege;
+		std::string nom;
+
+		Carte* c1;
+		Carte* c2;
+	public:
+		Joueur(std::string nom);
+		~Joueur();
+		void ajouter_jeu(Jeu* j);
+};
+
+#endif

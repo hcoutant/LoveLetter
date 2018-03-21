2a491c4abd0b936563b87e133e4b7c314e4478b9
hcoutant <coutanth@gmail.com>
2018-03-22 00:44:40 +0100

    Modification main

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include "jeu.h"
+
 
 int main(void)
 {
-	std::cout << "Love Letter" << std::endl;
+	Joueur *j1 = new Joueur("Jean");
+	Joueur *j2 = new Joueur("Robert");
+
+	Jeu *j = new Jeu(j1,j2);
+	
+	j1->ajouter_jeu(j);
+	j2->ajouter_jeu(j);
+
+	
+
 	return 0;
 }

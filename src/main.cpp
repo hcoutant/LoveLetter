07a1c9ad480c6c367f2d8de9555739421848f2e9
Hugo Coutant <coutanth@gmail.com>
2018-03-27 15:49:46 +0200

    petit main de test

--- src/main.cpp
+++ src/main.cpp
@@ -14,9 +14,41 @@ int main(void)
 
 	j->initialisation();
 	
-	std::cout << j1->getCarteMg()->getType() << std::endl;
 
-	std::cout << j2->getCarteMg()->getType() << std::endl;
+	j1->setCarteMg(new Baron());
+	j1->setCarteMd(new Garde());
+
+	std::cout << "Joueur 1" << std::endl;
+	std::cout << j1->getCarteMg()->affiche() << std::endl;
+	std::cout << j1->getCarteMd()->affiche() << std::endl;
+
+
+	std::cout << std::endl << "Joueur 2" << std::endl;
+	std::cout << j2->getCarteMg()->affiche() << std::endl;
+
+	
+	j1->jouerCarteMg(j2);
+
+	std::cout << std::endl << "Joueur 1" << std::endl;
+	
+	if(j1->getCarteMg() != nullptr)
+	        std::cout << j1->getCarteMg()->affiche() << std::endl;
+	if(j1->getCarteMd() != nullptr)
+		std::cout << j1->getCarteMd()->affiche() << std::endl;
+
+
+	
+
+
+	  std::cout << std::endl << "Joueur 2" << std::endl;
+
+
+	if(j2->getCarteMg() != nullptr)
+                std::cout << j2->getCarteMg()->affiche() << std::endl;
+        if(j2->getCarteMd() != nullptr)
+                std::cout << j2->getCarteMd()->affiche() << std::endl;
+
+
 
 	return 0;
 }

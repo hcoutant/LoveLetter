34e65cdc24b01043a5f4890892a5c6918f7eaf1e
Hugo Coutant <coutanth@gmail.com>
2018-04-22 17:10:11 +0200

    Commentaires

--- src/joueur.h
+++ src/joueur.h
@@ -9,45 +9,46 @@ class Jeu; //Eviter les inclusions cyclique
 
 class Joueur {
 	private:
-		Jeu* j;
-		bool en_vie;
-		bool est_protege;
-		std::string nom;
-		int score;
+		Jeu* j; //Jeu auquel le joueur est attaché
+		bool en_vie; //Indique si le joueur est en vie
+		bool est_protege; //Indique si le joueur est protégé
+		std::string nom; //Indique le pseudo du joueur
+		int score; //Indique le score du joueur
 
-		Carte* c1;
-		Carte* c2;
+		Carte* c1; //La carte en main gauche du joueur
+		Carte* c2; //La carte en main droite du joueur
 	public:
-		Joueur(std::string nom);
-		~Joueur();
-		void ajouterJeu(Jeu* j);
+		Joueur(std::string nom); //Constructeur 
+		~Joueur(); //Destructeur
+		void ajouterJeu(Jeu* j); //setter pour le jeu
 		
 		//int, pas retour parce que ... le c++ \o/
-		int jouerCarteMg(Joueur* jo);
-		int jouerCarteMd(Joueur* jo);
+		int jouerCarteMg(Joueur* jo); //Joue la carte en main gauche
+		int jouerCarteMd(Joueur* jo); //Joue la carte en main droite
 
-		int jouerCarteMg(Joueur *jo, Carte* c);
+		//Même chose que précédement, mais on garde un pointeur pour retourner ou passer une carte (Pretre et Garde)
+		int jouerCarteMg(Joueur *jo, Carte* c); 
 		int jouerCarteMd(Joueur *jo, Carte* c);
 
-		bool estVivant();
-		void setVivant(bool b);
+		bool estVivant(); //Getter pour savoir si un joueur est vivant
+		void setVivant(bool b); //setter vivant
 	
-		bool estProtege();
-		void setProtege(bool b);
+		bool estProtege(); //getter proteger
+		void setProtege(bool b); //setter proteger
 		
-		Carte* getCarteMg();
-		void setCarteMg(Carte* c);
+		Carte* getCarteMg(); //getter pour carte mg
+		void setCarteMg(Carte* c); //setter pour carte mg
 
-		Carte* getCarteMd();
-		void setCarteMd(Carte* c);
+		Carte* getCarteMd(); //getter pour carte md
+		void setCarteMd(Carte* c); //setter pour carte mg
 
-		void defausser();
-        Carte* piocher();
+		void defausser(); //Defausse ses cartes 
+        	Carte* piocher(); //Pioche une carte
 
-		std::string getNom();
+		std::string getNom(); //Retourne le pseudo du joueur
 	
-		int getScore();
-		void setScore(int);
+		int getScore(); //Getter pour le score
+		void setScore(int); //Setter pour le score
 };
 
 #endif
